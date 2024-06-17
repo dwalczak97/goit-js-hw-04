913b89e300fb0fdafaa7fc845b7efb9680c3453c
dwalczak97 <d.melon@op.pl>
2024-06-17 17:32:39 +0200

    task 1

---
+++ task-1.js
@@ -0,0 +1,20 @@
+function isEnoughCapacity(products, containerSize) {
+  let sumProducts = 0;
+  const listValue = Object.values(products);
+
+  for (const num of listValue) {
+    sumProducts += num;
+  }
+
+  if (sumProducts < containerSize) {
+    return true;
+  } else return false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
